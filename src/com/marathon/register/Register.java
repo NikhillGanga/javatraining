113b0f850e79f087535f2f0a89090916b7a4052d
nikhilganga006 <nikhil.y@goodworklabs.com>
2026-02-06 12:44:43 +0530

    Exception

---
+++ src/com/marathon/register/Register.java
@@ -0,0 +1,40 @@
+package com.marathon.register;
+
+public class Register {
+	
+	String name;
+	String gender;
+	int age;
+	String contact;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		
+		this.age = age;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		
+		this.contact = contact;
+	}
+	
+	
+	
+
+}
