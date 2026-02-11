cd67a008db7e00b690e6584ed708f7a3fab6a41c
nikhilganga006 <nikhil.y@goodworklabs.com>
2026-02-11 13:13:24 +0530

    Feb11

--- src/com/java8/payment/Driver.java
+++ src/com/java8/payment/Driver.java
@@ -17,15 +17,15 @@ public class Driver {
 		String paymentId=s.next();
 		System.out.print("Enter Amount:");
 		double amount=s.nextDouble();
-		Predicate<Double> predicate=n->n>0;
+		Predicate<Double> predicate=n->n<=0;
 		String status = "";
 
 		Consumer<String> consumer=n->System.out.println("Log:"+n);
-		if (predicate.test(amount)) {
+		if (!predicate.test(amount)) {
 		    status = "Successful";
 		    consumer.accept(status);
 		}
-		else if(!predicate.test(amount))
+		else if(predicate.test(amount))
 		{
 			status="Failed";
 			consumer.accept(status);
@@ -40,7 +40,13 @@ public class Driver {
 
                 String referenceId=generate.get();
          Payment payment=new Payment(paymentId, amount, status,referenceId);
-         Function<Payment, String>function=n->n.toString();
+         Function<Payment, String>function=n->
+         {
+        	 if(n.getStatus()=="Successful")
+        		return n.toString();
+        	 else
+        		 return "Payment Failed";
+         };
          System.out.println("=========================");
          System.out.println(function.apply(payment));
 		
